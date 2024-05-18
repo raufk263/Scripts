e4456cf60e664bcdad3b16877c0d1b06f314f4ec
RAUF KHAN <74665684+raufk263@users.noreply.github.com>
2024-05-18 15:42:31 +0530

    Version_1

---
+++ BashScripts/Delete_Screenshot_After_7Days.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Directory where screenshots are stored, Modify it according to your file location.
+screenshot_dir="/home/rauf/Pictures/Screenshots/"
+
+# Delete screenshots older than 7 days
+find "$screenshot_dir" -type f -name "*.png" -mtime +7 -exec rm {} \;
+
+#---------------------------------------------------------------------------------------------------------README----------------------------------------------------------------------------------------------
+#This is bash script created to delete an screesnhot older than 7 days. It's an demonstration of script that is used in screesnshot case but the real life scenario can be deleting log files older than 7 days
+#or X no. of days
+
+#Step 1 - Create an bash script named DeleteSS.sh
+
+#Step 2 - Schedule an cron Job which will be executed every 7 day to execute the script.
+
+#For creating cron Job to be executed every 7 day.
+
+# Enter command on termianl crontab -e and paste the 2nd line in crontab
+
+# $ crontab -e
+# 0 0 */7 * * /home/rauf/BashScripts/DeleteSS.h
+
+#-----------------------------------------------------------------------------------------------THANK YOU !!---------------------------------------------------------------------------------------------------
